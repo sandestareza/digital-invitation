dd022d02733386c283b23e9b90977f72e0f46853
sandesta reza <78458304+sandestareza@users.noreply.github.com>
2025-10-23 11:03:03 +0700

    Update layout.tsx screen

--- src/app/(pages)/admin/layout.tsx
+++ src/app/(pages)/admin/layout.tsx
@@ -5,7 +5,7 @@ export default function AdminLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<div className="container mx-auto p-4 bg-gray-900 text-cream h-screen">
+		<div className="p-4 bg-gray-900 text-cream w-full min-h-screen">
 			<div className="flex flex-col items-center mb-6">
 				{children}
 			</div>
